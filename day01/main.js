b49d6fe838a380d88b34d25655ab252557f93b0b
Cami Blanch <caminblanch@gmail.com>
2019-12-04 22:57:00 -0700

    Don't use a class. It only adds complexity

--- day01/main.js
+++ day01/main.js
@@ -1,22 +1,15 @@
 const readFile = require("../readFile");
 
-class Module {
-	constructor(mass = 0) {
-		this.mass = mass;
-	}
-
-	calcFuelNeeded() {
-		const fuel = Math.floor(this.mass / 3) - 2;
-		return fuel < 0 ? 0 : fuel;
-	}
+function calcFuelNeeded(mass) {
+	const fuel = Math.floor(mass / 3) - 2;
+	return fuel < 0 ? 0 : fuel;
 }
 
 const moduleMasses = readFile("input.txt");
 let partOneFuel = 0;
 
 moduleMasses.forEach(mass => {
-	const module = new Module(mass);
-	partOneFuel += module.calcFuelNeeded();
+	partOneFuel += calcFuelNeeded(mass);
 });
 
 console.log("Part one: " + partOneFuel);
