170a39d00161edfd0df38fbe3718a2396b26f23d
Cami Blanch <caminblanch@gmail.com>
2019-12-04 23:11:30 -0700

    Successful part 2. Recursion is hard

--- day01/main.js
+++ day01/main.js
@@ -5,12 +5,28 @@ function calcFuelNeeded(mass) {
 	return fuel < 0 ? 0 : fuel;
 }
 
+function manyFuel(fuel) {
+	const fuelForFuel = calcFuelNeeded(fuel);
+	let fuelTotal = fuelForFuel;
+	if (fuelForFuel > 0) {
+		fuelTotal += manyFuel(fuelForFuel);
+	}
+	return fuelTotal;
+}
+
 const moduleMasses = readFile("input.txt");
 let partOneFuel = 0;
+let partTwoFuel = 0;
 
 moduleMasses.forEach(mass => {
-	partOneFuel += calcFuelNeeded(mass);
+	const fuel = calcFuelNeeded(mass);
+	partOneFuel += fuel;
+
+	partTwoFuel += manyFuel(fuel);
 });
 
+partTwoFuel += partOneFuel;
+
 console.log("Part one: " + partOneFuel);
+console.log("Part two: " + partTwoFuel);
 
