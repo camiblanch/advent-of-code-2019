bb59119bb9ed00295e489726ee3b5065eea4f208
Cami Blanch <caminblanch@gmail.com>
2019-12-04 23:47:26 -0700

    Day 2 Part 1 solution

--- day02/main.js
+++ day02/main.js
@@ -2,4 +2,45 @@ function readFile(file) {
 	const fs = require("fs");
 	return fs.readFileSync(file).toString().split(",").map(Number);
 }
-const opcodes = readFile("input.txt");
+
+function operate(opcodeIndex) {
+	switch (opcodes[opcodeIndex]) {
+		case 1:
+			// TODO: add
+			add(getPositions(opcodeIndex));
+			opcodeIndex += 4;
+			operate(opcodeIndex);
+			break;
+		case 2:
+			multiply(getPositions(opcodeIndex));
+			opcodeIndex += 4;
+			operate(opcodeIndex);
+			break;
+		case 99:
+			return;
+		default:
+			console.error("THIS IS FINE");
+			return;
+	}
+}
+
+function getPositions(opcodeIndex) {
+	return {
+		x: opcodes[opcodes[opcodeIndex + 1]],
+		y: opcodes[opcodes[opcodeIndex + 2]],
+		answerIndex: opcodes[opcodeIndex + 3],
+	};
+}
+
+function add({x, y, answerIndex}) {
+	opcodes[answerIndex] = x + y;
+}
+
+function multiply({x, y, answerIndex}) {
+	opcodes[answerIndex] = x * y;
+}
+
+let opcodes = readFile("input.txt");
+operate(0);
+console.log(opcodes[0]);
+
