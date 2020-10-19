0ea76af984ff929be6b29c5d6834d85f66b58918
Cami Blanch <caminblanch@gmail.com>
2020-10-19 16:38:24 -0600

    Less looping, faster run. Still not ideal

--- day03/main.js
+++ day03/main.js
@@ -5,14 +5,26 @@ const readFile = file => {
 	return lines.map(line => line.split(","));
 };
 
+let taxi0Positions = [];
+let taxi1Positions = [];
+let intersections = [];
+
 const moves = {
 	"U": [0, 1],
 	"D": [0, -1],
-	"L": [1, 0],
-	"R": [-1, 0],
+	"L": [-1, 0],
+	"R": [1, 0],
 };
 
-const findAllPositions = instructions => {
+const isIntersection = (position) => {
+	const index = taxi0Positions.findIndex((taxi0Position) => {
+		return taxi0Position[0] === position[0] && taxi0Position[1] === position[1];
+	});
+
+	return index > -1;
+};
+
+const findAllPositionsAndIntersections = (instructions, isTaxi1) => {
 	let currPosition = [0, 0];
 	let allPositions = [];
 	instructions.forEach((instruction) => {
@@ -25,26 +37,35 @@ const findAllPositions = instructions => {
 			currPosition[0] = currPosition[0] + move[0];
 			currPosition[1] = currPosition[1] + move[1];
 			allPositions.push([...currPosition]);
+
+			if (isTaxi1 && isIntersection(currPosition)) {
+				intersections.push([...currPosition]);
+			}
 		}
 	});
 
 	return allPositions;
 };
 
-const findIntersections = ([taxi0Positions, taxi1Positions]) => {
-
-	return taxi0Positions.filter(taxi0Position => {
-		return taxi1Positions.find((taxi1Position) => taxi0Position[0] === taxi1Position[0] && taxi0Position[1] === taxi1Position[1]);
+const findShortestManhattanDistance = (intersections) => {
+	let shortest = 0;
+	intersections.forEach(([x, y]) => {
+		const manhattanDistance = Math.abs(x) + Math.abs(y);
+		if (shortest === 0 || manhattanDistance < shortest) {
+			shortest = manhattanDistance;
+		}
 	});
+
+	return shortest;
 };
 
 const main = () => {
 	const taxis = readFile("input.txt");
-	const positions = taxis.map(taxi => {
-		return findAllPositions(taxi);
-	});
+	taxi0Positions = findAllPositionsAndIntersections(taxis[0]);
+	taxi1Positions = findAllPositionsAndIntersections(taxis[1], true);
 
-	const intersections = findIntersections(positions);
+	const manhattanDistance = findShortestManhattanDistance(intersections);
+	console.log(manhattanDistance);
 };
 
 main();
