edbe1cac31f1ca28c0f73b6eadf4dcb0fc019a1c
Cami Blanch <caminblanch@gmail.com>
2020-10-22 17:18:24 -0600

    Adds logic for part 2 requirements

--- day04/main.js
+++ day04/main.js
@@ -13,6 +13,31 @@ const containsAtLeastOneDigitPair = (password) => {
 	return false;
 };
 
+const containsOneEvenDigitPair = (password) => {
+	for (let i = 0; i < password.length;) {
+		let charToMatch = password[i];
+
+		if (charToMatch === password[i + 1]) {
+			if (charToMatch === password[i + 2]) {
+				if (charToMatch === password[i + 3]) {
+					if (charToMatch === password[i + 4]) {
+						i = i + 5;
+					} else {
+						i = i + 4;
+					}
+				} else {
+					i = i + 3;
+				}
+			} else {
+				return true;
+			}
+		} else {
+			i++;
+		}
+	}
+	return false;
+};
+
 const digitsNeverDecrease = (password) => {
 	for (let i = 0; i < password.length - 1; i++) {
 		if (parseInt(password.charAt(i)) > parseInt(password.charAt(i + 1))) {
@@ -30,6 +55,24 @@ const passesPartOneFacts = (number) => {
 	}
 
 	if (!containsAtLeastOneDigitPair(password)) {
+		return false;
+	}
+
+	if (!digitsNeverDecrease(password)) {
+		return false;
+	}
+
+	return true;
+};
+
+const passesPartTwoFacts = (number) => {
+	const password = number.toString();
+
+	if (password.length !== 6) {
+		return false;
+	}
+
+	if (!containsOneEvenDigitPair(password)) {
 		return false;
 	}
 
@@ -58,7 +101,7 @@ const partTwoMain = () => {
 	const matches = [];
 
 	for (let i = range[0]; i <= range[1]; i++) {
-		if (passesPartOneFacts(i)) {
+		if (passesPartTwoFacts(i)) {
 			matches.push(i);
 		}
 	}
