8919366e07f7f303f3487f61579543bf0846c410
Cami Blanch <caminblanch@gmail.com>
2020-10-22 15:35:48 -0600

    Adds loop to check matches

--- day04/main.js
+++ day04/main.js
@@ -11,6 +11,16 @@ const passesFacts = (password) => {
 
 const main = () => {
 	const range = readFile("input.txt");
+	const matches = [];
+
+	for (let i = range[0]; i <= range[1]; i++) {
+		if (passesFacts(i)) {
+			matches.push(i);
+		}
+	}
+
+	console.log(matches);
+	console.log("Num matches: ", matches.length);
 };
 
 main();
